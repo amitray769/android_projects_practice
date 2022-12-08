dc3efec0fa2ff91b1d667007e09045d02956fafe
Amit Ray <amit.ray@brokernetwork.app>
2022-12-08 23:08:04 +0530

    Button shapes basic components

---
+++ AndroidBasicComponentsManipulation/app/src/main/java/com/example/androidbasiccomponentsmanipulation/buttons/ButtonShapes.kt
@@ -0,0 +1,12 @@
+package com.example.androidbasiccomponentsmanipulation.buttons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidbasiccomponentsmanipulation.R
+
+class ButtonShapes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button_shapes)
+    }
+}
---
+++ AndroidBasicComponentsManipulation/app/src/main/java/com/example/androidbasiccomponentsmanipulation/imageview/ImageViewManipulations.kt
@@ -0,0 +1,12 @@
+package com.example.androidbasiccomponentsmanipulation.imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidbasiccomponentsmanipulation.R
+
+class ImageViewManipulations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_view_manipulations)
+    }
+}
